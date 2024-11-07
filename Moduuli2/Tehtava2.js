b34f4bcea4f2b8abb45464810a132de6f4e2dd4b
tuukka <tuukkauu@metropolia.fi>
2024-11-07 14:58:27 +0200

    Tehtävä 2 tehty

---
+++ Moduuli2/Tehtava2.js
@@ -0,0 +1,21 @@
+'use script'
+
+const name = [];
+
+const how_many = Number(prompt('How many participants?'))
+
+
+for (let i = 1; i <= how_many; i++) {
+  const participantName = prompt('Give a name of the participant:');
+  name.push(participantName)
+}
+
+let ordered_list = '<ol>';
+
+for (let i = 0; i < name.length; i++) {
+    ordered_list += `<li>${name[i]}</li>`;
+}
+
+ordered_list += '</ol>'
+
+document.getElementById('result').innerHTML = ordered_list;
