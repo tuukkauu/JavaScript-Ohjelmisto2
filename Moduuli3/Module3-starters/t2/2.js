4dc3d101e5f626ed5627aa147fb5396a74dfa6ed
tuukka <tuukkauu@metropolia.fi>
2024-11-12 21:34:14 +0200

    t2 kesken

---
+++ Moduuli3/Module3-starters/t2/2.js
@@ -0,0 +1,12 @@
+'use script'
+
+let unordlist = '<ul>'
+
+unordlist += '<li>First item</li>'
+unordlist += '<li>Second item</li>'
+unordlist += '<li>Third item</li>'
+
+unordlist += '</ul>';
+
+
+document.createElement('target')
