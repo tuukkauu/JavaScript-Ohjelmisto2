044fa87d28024f0fa42c3a385e03dceec8c57313
tuukka <tuukkauu@metropolia.fi>
2024-11-15 19:49:54 +0200

    t7 kesken

---
+++ Moduuli3/Module3-starters/t7/7.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+const pic = document.querySelector('#target');
+
+pic.addEventListener('mouseover', function() {
+  document.querySelector('#trigger', src='picB.jpg');
+})
+
+pic.addEventListener('mouseout', function() {
+  document.querySelector('#target')
+})
+
+
