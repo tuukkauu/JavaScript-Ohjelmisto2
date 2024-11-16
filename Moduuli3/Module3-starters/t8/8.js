7acec0e4783f8ed9e542961bb585d2c33db3f230
tuukka <tuukkauu@metropolia.fi>
2024-11-17 00:11:03 +0200

    t8 valmis

--- Moduuli3/Module3-starters/t8/8.js
+++ Moduuli3/Module3-starters/t8/8.js
@@ -1,5 +1,34 @@
 'use strict';
 
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
+const num1input = document.querySelector('#num1');
+const num2input = document.querySelector('#num2');
+const operation = document.querySelector('#operation');
+const calcButton = document.querySelector('#start');
+const resultElement = document.querySelector('#result');
+
+calcButton.addEventListener('click', function() {
+  const num1 = Number(num1input.value);
+  const num2 = Number(num2input.value);
+  const selectedOperation = operation.value;
+  let result;
+
+  if (selectedOperation === 'add') {
+  result = num1 + num2;
+} else if (selectedOperation === 'sub') {
+    result = num1 - num2;
+  } else if (selectedOperation === 'multi') {
+    result = num1 * num2;
+  } else if (selectedOperation === 'div') {
+    if (num2 === 0) {
+    result = 'Number cannot be 0.'
+    } else {
+      result = num1 / num2;
+    }
+  }
+
+  resultElement.textContent = `Result: ${result}`;
+  });
+
+
+
 
